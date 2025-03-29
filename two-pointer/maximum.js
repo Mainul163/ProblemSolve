b4a2b410e7f0e5ebb0a21d39d6b72df15aaef20e
Mainul163 <mainulisraf163@gmail.com>
2025-03-30 00:31:05 +0600

    new problem solve

---
+++ two-pointer/maximum.js
@@ -0,0 +1,21 @@
+function maxNumber(array, k) {
+  let sum = 0;
+  let maximum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += array[i];
+  }
+  maximum = sum;
+
+  for (let i = k; i < array.length; i++) {
+    sum += array[i] - array[i - k];
+    if (maximum < sum) {
+      maximum = sum;
+    }
+  }
+
+  return maximum;
+}
+
+const array = [1, 3, -1, -3, 5, 3, 6, 7];
+
+console.log(maxNumber(array, 3));
